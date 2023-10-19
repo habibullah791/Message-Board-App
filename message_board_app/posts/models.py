120e6b617fabadb6f5f3f82b3e7191fc18098d64
habibullah791 <habibullahshahid001@gmail.com>
2023-10-18 17:12:49 -0700

    Create - model, authenticate model, Create - superuser admin, Create posts and Add - templates and link them and diaplay posts using templates

--- message_board_app/posts/models.py
+++ message_board_app/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    message = models.TextField()
+    
+    
+    def __str__(self) -> str:
+        return self.message[:90]
--- message_board_app/posts/tests.py
+++ message_board_app/posts/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+# class PostTest(TestCase):
+    
