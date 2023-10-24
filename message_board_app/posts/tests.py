b5d116ff0dd7b3d4d04b9ccf1e6538b60dd0d61c
habibullah791 <habibullahshahid001@gmail.com>
2023-10-23 22:46:32 -0700

    Added comments

--- message_board_app/posts/tests.py
+++ message_board_app/posts/tests.py
@@ -2,34 +2,45 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import Post
 
-
 class PostTest(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
+        """
+        Set up test data for the Post model.
+        """
         cls.post = Post.objects.create(
             id=1,
             name="test",
             message="test",
         )
-    
-    
-    def test_model_content(self):
+
+    def test_model_content(self) -> None:
+        """
+        Test the Post model's attributes.
+        """
         self.assertEqual(self.post.id, 1)
         self.assertEqual(self.post.name, "test")
         self.assertEqual(self.post.message, "test")
-    
-    
-    def test_url_exist_at_location(self):
+
+    def test_url_exist_at_location(self) -> None:
+        """
+        Test if the URL '/' exists and returns a 200 status code.
+        """
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-    
-    def test_url_not_available_by_name(self):
+
+    def test_url_not_available_by_name(self) -> None:
+        """
+        Test if a URL with the name 'home' + '1' does not exist (not a 200 status code).
+        """
         response = self.client.get(reverse("home") + "1")
         self.assertNotEqual(response.status_code, 200)
-    
-    def test_home_page(self):
+
+    def test_home_page(self) -> None:
+        """
+        Test the home page URL, template used, and content.
+        """
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "test")
-        
--- message_board_app/posts/urls.py
+++ message_board_app/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HomePageView
 
+
+# add the URL pattern for the home page
 urlpatterns = [
     path("", HomePageView.as_view(), name="home")
 ]
